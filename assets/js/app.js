82f5460667b648b11b575498029e9e02a7400909
suitto321 <suittoscience@gmail.com>
2019-01-25 14:37:57 -0500

    add code to app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,85 @@
 // @TODO: YOUR CODE HERE!
+// Store width and height parameters 
+let svgWidth = 960;
+let svgHeight = 500;
+
+// Set svg margins
+let margin = {
+    top: 20,
+    right: 40,
+    bottom: 80,
+    left: 100
+  };
+
+// Create the width and height based svg margins and parameters
+let width = svgWidth - margin.left - margin.right;
+let height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold the scatter plot,
+// and shift the latter by left and top margins.
+let svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+// Append an SVG group
+let chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Initial Params
+const chosenXAxis = "poverty";
+
+// function used for updating x-scale var upon click on axis label
+function xScale(stateData, chosenXAxis) {
+  // create scales
+  let xLinearScale = d3.scaleLinear()
+    .domain([d3.min(stateData, d => d[chosenXAxis]) * 0.8,
+      d3.max(stateData, d => d[chosenXAxis]) * 1.2
+    ])
+    .range([0, width]);
+
+  return xLinearScale;
+}
+// function used for updating xAxis var upon click on axis label
+function renderAxes(newXScale, xAxis) {
+    let bottomAxis = d3.axisBottom(newXScale);
+  
+    xAxis.transition()
+      .duration(1000)
+      .call(bottomAxis);
+  
+    return xAxis;
+}
+// function used for updating circles group with a transition to
+// new circles
+function renderCircles(circlesGroup, newXScale, chosenXaxis) {
+
+  circlesGroup.transition()
+    .duration(1000)
+    .attr("cx", d => newXScale(d[chosenXAxis]));
+
+  return circlesGroup;
+}
+// function used for updating circles group with new tooltip
+function updateToolTip(chosenXAxis, circlesGroup) {
+
+  if (chosenXAxis === "poverty") {
+    var label = "Poverty:";
+  }
+  else {
+    var label = "# of Albums:";
+  }
+
+  var toolTip = d3.tip()
+    .attr("class", "tooltip")
+    .offset([80, -60])
+    .html(function(d) {
+      return (`${d.rockband}<br>${label} ${d[chosenXAxis]}`);
+    });
+
+  circlesGroup.call(toolTip);
+
+  circlesGroup.on("mouseover", function(data) {
+    toolTip.show(data);
+  })
